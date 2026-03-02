5828754d1cb6b8a5aa4900b090e7592461b0fd17
Prajwal Gowda <gowdaprajwal418@gmail.com>
2026-03-02 09:32:15 +0530

    Create DB tables on startup

--- database.py
+++ database.py
@@ -2,6 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 import os
+from models import Base
+from sqlalchemy import create_engine
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+Base.metadata.create_all(bind=engine)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "movies.db")
